cc007fa8afcc95b84f84767d1a3833abda7b31c4
kreation123 <71356383+kreation123@users.noreply.github.com>
2021-08-31 20:06:23 -0400

    Add files via upload

---
+++ CLass 100/carBluePrint.py
@@ -0,0 +1,13 @@
+class Car():
+    def __init__(self,color, modle , type , company , weight  ):
+        self.color=color
+        self.modle =modle
+        self.type = type
+        self.company = company
+        self.weight = weight
+def start(self):
+    print('car has beed started') 
+def stop(self):
+    print('the car has been stoped')
+bmw = Car('red', '12345678909','suv','bmw','1 ton')
+print(bmw.start())
